a7e737e48d04b229ca1f0c639185aee325269f24
saguywalker <guyguy252@gmail.com>
2019-05-20 19:51:58 +0700

    Manually json-rpc with arguments

--- key.go
+++ key.go
@@ -9,6 +9,10 @@ import (
 	"github.com/KeisukeYamashita/jsonrpc"
 )
 
+const (
+	StreamName string = "competencies"
+)
+
 func main() {
 	jsonFile, err := os.Open("config.json")
 	if err != nil {
@@ -24,55 +28,32 @@ func main() {
 	rpcClient := jsonrpc.NewRPCClient(url)
 	rpcClient.SetBasicAuth(config.Username, config.Password)
 
-	jsonData := []byte(`{"params":["competencies",{"keys":["2","2018"]}]}`)
+	/*jsonData := []byte(`{"params":["competencies",{"keys":["2","2018"]}]}`)
 	var data map[string]interface{}
 	json.Unmarshal(jsonData, &data)
-	for k, v := range data {
-		switch v := v.(type) {
-		case string:
-			fmt.Println(k, v, "(string)")
-		case float64:
-			fmt.Println(k, v, "(float)")
-		case []interface{}:
-			fmt.Println(k, v, "(array)")
-			for i, u := range v {
-				fmt.Println("	", i, u)
-				switch u := u.(type) {
-				case map[string]interface{}:
-					for kk, vv := range u {
-						fmt.Println("		", kk, vv, "(sub map)")
-						vvv := vv.([]interface{})
-						for j, jj := range vvv {
-							fmt.Println("			", j, jj)
-						}
-					}
-				case []interface{}:
-					for ii, vv := range u {
-						fmt.Println("		", ii, vv, "(sub array)")
-					}
-				default:
-					fmt.Println("		", v, "(sub unknown)")
-				}
-			}
-		default:
-			fmt.Println(k, v, "(unknown)")
-		}
-	}
-	fmt.Println("***********************************")
 
 	mapData := make(map[string][]string)
 	mapData["keys"] = []string{"2", "2018"}
 	json2, _ := json.Marshal(mapData)
 	var data2 [2]interface{}
-	data2[0] = "competencies"
+	data2[0] = `competencies`
 	data2[1] = `'` + string(json2) + `'`
+	data2[1] = mapData
 	fmt.Println(data2[1])
 	fmt.Println("liststreamqueryitems", data2[0], data2[1])
-	resp, err := rpcClient.Call("liststreamqueryitems", data2)
+	fmt.Println("****************************")*/
+	tmp := [2]interface{}{StreamName, map[string][]string{"keys": []string{"59130500211"}}}
+	listQueryCommand := Payload{
+		Method: "liststreamqueryitems",
+		Params: tmp,
+	}
+	fmt.Println("liststreamqueryitems", listQueryCommand.Params[0], listQueryCommand.Params[1])
+	resp, err := rpcClient.Call("liststreamqueryitems", listQueryCommand.Params[0], listQueryCommand.Params[1])
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(resp)
+	fmt.Println("****************************")
 
 }
 
@@ -85,9 +66,6 @@ type AccountConfig struct {
 }
 
 type Payload struct {
-	Method string `json:"method"`
-	Params struct {
-		StreamName string                 `json:"stream"`
-		Keys       map[string]interface{} `json:"keys"`
-	} `json:"params"`
+	Method string         `json:"method"`
+	Params [2]interface{} `json:"params"`
 }
