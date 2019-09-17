5ae55af8d26537b1472e52579449c303a94d27e8
Nongtiny <tinyutb3612@gmail.com>
2019-09-18 01:45:22 +0700

    vue config

--- ui/admin-sc/vue.config.js
+++ ui/admin-sc/vue.config.js
@@ -1,3 +1,18 @@
+const path = require("path");
+
 module.exports = {
-	publicPath: "/admin"
+	css: {
+		extract: true,
+		sourceMap: true
+	},
+	outputDir: path.resolve(__dirname, "dist", "admin"),
+	publicPath: "/admin",
+	configureWebpack: {
+		resolve: {
+			alias: {
+				"~": path.resolve(__dirname, "src"),
+				"@": path.resolve(__dirname, "src")
+			}
+		}
+	}
 };
