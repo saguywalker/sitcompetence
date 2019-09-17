418ce54398a1e9ba15de9946e0022fe32e7f7373
saguywalker <guyguy252@gmail.com>
2019-09-18 00:43:58 +0700

    serve index.html

--- cmd/serve.go
+++ cmd/serve.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"sync"
 	"time"
 
@@ -27,9 +28,8 @@ func serveAPI(ctx context.Context, api *api.API) {
 	router := mux.NewRouter()
 	api.Init(router.PathPrefix("/api").Subrouter())
 
-	//router.PathPrefix("/").HandlerFunc(IndexHandler("student/dist/index.html"))
-	router.PathPrefix("/").HandlerFunc(HelloHandler())
-	router.PathPrefix("/admin/").HandlerFunc(IndexHandler("admin-c/dist/index.html"))
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("ui/admin-sc/dist/")))
+	router.PathPrefix("/admin").HandlerFunc(IndexHandler("ui/admin-sc/dist/index.html"))
 
 	s := &http.Server{
 		Addr:        fmt.Sprintf(":%d", api.Config.Port),
@@ -53,19 +53,11 @@ func serveAPI(ctx context.Context, api *api.API) {
 	<-done
 }
 
-// IndexHandler serves index files
 func IndexHandler(entrypoint string) func(w http.ResponseWriter, r *http.Request) {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, entrypoint)
 	}
 	return http.HandlerFunc(fn)
-}
-
-func HelloHandler() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("fuck the police\n"))
-	}
-
 }
 
 var serveCmd = &cobra.Command{
@@ -105,6 +97,32 @@ var serveCmd = &cobra.Command{
 		wg.Wait()
 		return nil
 	},
+}
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(h.staticPath, path)
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
 func init() {
