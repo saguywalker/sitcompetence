615cc5f28ac1aeeb3f6c26206e0f5491e5e3c625
Nongtiny <tinyutb3612@gmail.com>
2019-09-17 17:42:17 +0700

    zz

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "github.com/saguywalker/sitcompetence/cmd"
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+
+	"gopkg.in/ldap.v3"
+)
 
 func main() {
-	cmd.Execute()
+	username := "lec01"
+	password := "JzNY5#2@gDuB"
+
+	bindusername := "lec01"
+	bindpassword := "JzNY5#2@gDuB"
+
+	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", "ld0620.sit.kmutt.ac.th", 636))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	// Reconnect with TLS
+	err = l.StartTLS(&tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// First bind with a read only user
+	err = l.Bind(bindusername, bindpassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Search for the given username
+	searchRequest := ldap.NewSearchRequest(
+		"uid=lec01,ou=People,ou=staff,dc=sit,dc=kmutt,dc=ac,dc=th",
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		fmt.Sprintf("(&(objectClass=organizationalPerson)(uid=%s))", username),
+		[]string{"dn"},
+		nil,
+	)
+
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(sr.Entries) != 1 {
+		log.Fatal("User does not exist or too many entries returned")
+	}
+
+	userdn := sr.Entries[0].DN
+
+	// Bind as the user to verify their password
+	err = l.Bind(userdn, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Rebind as the read only user for any further queries
+	err = l.Bind(bindusername, bindpassword)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
