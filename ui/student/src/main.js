bbe5dc2643621e150c172961b7286a2a97e9d434
Nongtiny <tinyutb3612@gmail.com>
2019-09-22 02:51:42 +0700

    login page

--- ui/student/src/main.js
+++ ui/student/src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import "@/styles/global.scss";
+import BaseInputText from "@/components/BaseInputText.vue";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.component("base-input-text", BaseInputText);
 
 new Vue({
 	router,
---
+++ ui/student/src/pages/Login.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="login">
+		<div
+			class="detail"
+			lg="8"
+		>
+			Hi
+		</div>
+		<div class="login-input">
+			<h4 class="title">
+				Login
+			</h4>
+			<base-input-text
+				type="text"
+				label="Username"
+				name="username"
+				size="lg"
+			/>
+		</div>
+	</div>
+</template>
+<style lang="scss">
+@import "@/styles/pages/login.scss";
+</style>
