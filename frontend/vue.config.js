10f3476f26c2a59a974e8fdbdcb922048904846d
Nongtiny <tinyutb3612@gmail.com>
2019-11-10 14:05:33 +0700

    fix build error

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
 	process.env.VUE_APP_API_URL = "https://10.4.56.22:3000/api";
 } else {
-	process.env.VUE_APP_API_URL = "https://localhost:3000/api";
+	process.env.VUE_APP_API_URL = "http://localhost:3000/api";
 }
 
 module.exports = {
