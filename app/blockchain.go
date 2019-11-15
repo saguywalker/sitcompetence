5d93e2ac8db14feca8af4c8caec8f532da5d10a1
saguywalker <saguywalker@protonmail.com>
2019-11-15 19:16:45 +0700

    log

--- app/blockchain.go
+++ app/blockchain.go
@@ -214,6 +214,8 @@ func (ctx *Context) VerifySignature(message []byte, signature, b64PubKey string)
 		return false, err
 	}
 
+	ctx.Logger.Infof("params: %s\nsha256: %x\nsignature: %x\n", message, hashed[:], sig)
+
 	isVerified := ed25519.Verify(pubKey, hashed[:], sig)
 
 	return isVerified, nil
