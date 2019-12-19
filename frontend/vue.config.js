541cd8ee6650fd090ced46e508c5d4ee2693ec97
Nongtiny <tinyutb3612@gmail.com>
2019-12-20 00:42:30 +0700

    fix env variable

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,6 @@
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-	process.env.VUE_APP_PROD_URL = "http://domainname:port";
-	process.env.VUE_APP_API_URL = "http://domainname:port/api";
-} else if (process.env.NODE_ENV === "localproduction") {
 	process.env.VUE_APP_PROD_URL = `http://${process.env.VUE_APP_BASE_IP}`;
 	process.env.VUE_APP_API_URL = `http://${process.env.VUE_APP_BASE_IP}:3000/api`;
 } else {
