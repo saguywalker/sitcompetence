ea30f17456bd68e0e56551e78d7ce25a96c4057c
Nongtiny <tinyutb3612@gmail.com>
2019-12-20 00:34:35 +0700

    fix cors

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,11 @@
 const path = require("path");
+
 if (process.env.NODE_ENV === "production") {
-	process.env.VUE_APP_PROD_URL = "http://localhost";
-	process.env.VUE_APP_API_URL = "http://localhost:3000/api";
+	process.env.VUE_APP_PROD_URL = "http://domainname:port";
+	process.env.VUE_APP_API_URL = "http://domainname:port/api";
+} else if (process.env.NODE_ENV === "localproduction") {
+	process.env.VUE_APP_PROD_URL = `http://${process.env.VUE_APP_BASE_IP}`;
+	process.env.VUE_APP_API_URL = `http://${process.env.VUE_APP_BASE_IP}:3000/api`;
 } else {
 	process.env.VUE_APP_PROD_URL = "http://localhost:8080";
 	process.env.VUE_APP_API_URL = "http://localhost:3000/api";
