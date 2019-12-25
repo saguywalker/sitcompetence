1ec13151a40d2e545fe13895eb546140e9901031
saguywalker <guyguy252@gmail.com>
2019-12-25 23:16:51 +0700

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git clone https://gist.github.com/b2062fbea1ba2e0d04f391301ccfcc1d.git
+cp b2062fbea1ba2e0d04f391301ccfcc1d/init.sql .
+cp b2062fbea1ba2e0d04f391301ccfcc1d/config.yaml .
+cp b2062fbea1ba2e0d04f391301ccfcc1d/.env ./frontend/.env
+rm -rf b2062*
