81897d831234685c4c295123c4985d7c598de9f8
saguywalker <saguywalker@protonmail.com>
2019-12-26 00:01:18 +0700

    request api from ipaddress

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,8 @@
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-	// process.env.VUE_APP_PROD_URL = `http://${process.env.VUE_APP_BASE_IP}`;
-	process.env.VUE_APP_PROD_URL = "http://localhost";
-	// process.env.VUE_APP_API_URL = `http://${process.env.VUE_APP_BASE_IP}/api`;
-	process.env.VUE_APP_API_URL = "http://localhost/api";
+	process.env.VUE_APP_PROD_URL = `http://${process.env.VUE_APP_BASE_IP}`;
+    process.env.VUE_APP_API_URL = `http://${process.env.VUE_APP_BASE_IP}/api`;
 } else {
 	process.env.VUE_APP_PROD_URL = "http://localhost:8080";
 	process.env.VUE_APP_API_URL = "http://localhost:3000/api";
